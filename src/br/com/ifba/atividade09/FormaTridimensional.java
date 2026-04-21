415ae7438fce0258b940145bb8fc50c0f4627e19
edina <edina@acer>
2026-04-21 20:39:57 -0300

    feat: adiciona classes abstratas para formas bidimensionais e tridimensionais

---
+++ src/br/com/ifba/atividade09/FormaTridimensional.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.atividade09;
+
+public abstract class FormaTridimensional extends Forma {
+    public abstract double obterArea();
+    public abstract double obterVolume();
+}
